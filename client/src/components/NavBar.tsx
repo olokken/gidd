7e008f303e579701ac21873530c9a89dc822bd1d
olokken <ole.lokken1@gmail.com>
2021-04-13 09:44:44 +0200

    Docker støtte

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,12 +1,22 @@
 import { AppBar, Button } from '@material-ui/core';
 import React from 'react'; 
+import { useHistory } from 'react-router';
 
 const Navbar = () => {
+    const history = useHistory(); 
+
+    const changeToMap = () => {
+        history.push('/Map');
+    }
+
+    const changeToHomePage = () => {
+        history.push('/HomePage');
+    }
 
     return(
         <AppBar style={{display:'flex'}}>
-            <Button>Hjemmesiden</Button>
-            <Button>Kart</Button>
+            <Button onClick={changeToHomePage}>Hjemmesiden</Button>
+            <Button onClick={changeToMap}>Kart</Button>
         </AppBar>
     )
 }
