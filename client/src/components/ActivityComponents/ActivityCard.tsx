e08460d04b874b6836b5a6a15b704e915d69bfdb
Iben Lind Dragesund <ibenld@stud.ntnu.no>
2021-04-14 15:11:32 +0200

    endra activitycard

--- client/src/components/ActivityComponents/ActivityCard.tsx
+++ client/src/components/ActivityComponents/ActivityCard.tsx
@@ -1,7 +1,8 @@
-import { Card, CardActionArea } from '@material-ui/core';
+import { Card, CardActionArea, CardMedia, CardContent, Typography, Grid, Avatar, makeStyles, Theme, createStyles, Chip } from '@material-ui/core';
 import React from 'react';
 import styled from 'styled-components';
 import Activity from '../../interfaces/Activity';
+
 
 const ActivityCard = ({
     ID,
@@ -14,11 +15,36 @@ const ActivityCard = ({
     level,
 }: Activity) => {
     return (
-        <Card>
+        <Card style={{maxWidth: '345px'}}>
             <CardActionArea>
-                <h4>{title}</h4>
-                <label>{description}</label>
-                <label>{time.toISOString}</label>
+                <CardMedia
+                component="img"
+                alt="Image related to the activity"
+                height="140"
+                image="../../assets/logo.png"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Tidspunkt: {time.toDateString}
+                    </Typography>
+                    <Chip variant="outlined" size="small" label="{x}/{y}" />
+                    <Chip variant="outlined" size="small" label={level} />
+                    <Typography gutterBottom variant="h5" component="h3">
+                        {title}
+                    </Typography>
+                    <Grid container wrap="nowrap" spacing={2}>
+                        <Grid item>
+                            <div className="tooltip"> {owner}
+                                <Avatar>{owner.charAt(0)}</Avatar>
+                            </div>
+                        </Grid>
+                        <Grid item xs>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                {description}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </CardContent>
             </CardActionArea>
         </Card>
     );
