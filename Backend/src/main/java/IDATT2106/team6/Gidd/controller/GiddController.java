2e5ddb597bca5d0a9e39138ddb20a2ec154b09cf
erlinssl <erlinssl@stud.ntnu.no>
2021-04-14 11:40:04 +0200

    Removed Spring Boot Security dependencies

--- Backend/src/main/java/IDATT2106/team6/Gidd/controller/GiddController.java
+++ Backend/src/main/java/IDATT2106/team6/Gidd/controller/GiddController.java
@@ -1,20 +1,30 @@
 package IDATT2106.team6.Gidd.controller;
 
 import IDATT2106.team6.Gidd.service.ActivityService;
-import IDATT2106.team6.Gidd.service.EquipmentService;
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GiddController {
-    @Autowired
-    private ActivityService activityService;
-    @Autowired
-    private EquipmentService equipmentService;
 
-    @GetMapping("/hello")
-    public String home(){
-        return "hello";
+    @Autowired
+    ActivityService activityService;
+
+    @GetMapping(value = "/test")
+    public ResponseEntity getTestPage() {
+        int[] a = {1, 2, 3};
+        return ResponseEntity
+            .created(URI
+                .create(String.format("%s", "hi")))
+            .body(a);
+    }
+
+    @GetMapping(value = "/somepage")
+    public String getpage() {
+        return "hello this is just a random page";
     }
 }
