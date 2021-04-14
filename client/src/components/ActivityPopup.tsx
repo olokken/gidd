f0a6217dc57c69c41f4c2d595f2742d811e2dd38
williamforbrigd <hansw0701@gmail.com>
2021-04-14 16:17:05 +0200

    Finished ActivityPopup

--- client/src/components/ActivityPopup.tsx
+++ client/src/components/ActivityPopup.tsx
@@ -1,5 +1,4 @@
 import React, { Dispatch, SetStateAction } from 'react';
-import Controls from './controls/Controls';
 import {
     Dialog,
     DialogTitle,
--- client/src/containers/HomePage.tsx
+++ client/src/containers/HomePage.tsx
@@ -1,17 +1,31 @@
-import React, { ChangeEvent, KeyboardEventHandler } from 'react';
+import React, { useState, ChangeEvent, KeyboardEventHandler } from 'react';
 import { TextField, Button } from '@material-ui/core';
 import styled from 'styled-components';
+import ActivityPopup from '../components/ActivityPopup';
+import ActivityForm from '../components/ActivityForm';
 
 const Container = styled.div`
     display: flex;
     justify-content: center;
-    margin-top:100px; 
+    margin-top: 100px;
 `;
 
 const HomePage = () => {
+    const [openPopup, setOpenPopup] = useState<boolean>(false);
     return (
         <Container>
             <h1>Dette er hjemmesiden</h1>
+            <button onClick={() => setOpenPopup(!openPopup)}>Popup</button>
+            <ActivityPopup
+                title="Legg til aktivitet"
+                openPopup={openPopup}
+                setOpenPopup={setOpenPopup}
+            >
+                <ActivityForm
+                    openPopup={openPopup}
+                    setOpenPopup={setOpenPopup}
+                />
+            </ActivityPopup>
         </Container>
     );
 };
