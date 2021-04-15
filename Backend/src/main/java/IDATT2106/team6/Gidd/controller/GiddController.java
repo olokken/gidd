07efd8ba26654ebb4396bec1e134dcd7bd91dd3f
Ingebrigt <ing.hovind@hotmail.com>
2021-04-15 13:04:45 +0200

    response header i login and register user

--- Backend/src/main/java/IDATT2106/team6/Gidd/controller/GiddController.java
+++ Backend/src/main/java/IDATT2106/team6/Gidd/controller/GiddController.java
@@ -7,7 +7,6 @@ import IDATT2106.team6.Gidd.models.Activity;
 import IDATT2106.team6.Gidd.service.UserService;
 import IDATT2106.team6.Gidd.service.ActivityService;
 
-import java.net.http.HttpHeaders;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,12 +14,15 @@ import IDATT2106.team6.Gidd.models.ActivityLevel;
 import IDATT2106.team6.Gidd.service.EquipmentService;
 import IDATT2106.team6.Gidd.service.UserService;
 import java.net.URI;
+
+import java.util.Iterator;
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -78,14 +80,19 @@ public class GiddController {
               map.get("password").toString(), map.get("firstName").toString(), map.get("surname").toString(),
               Integer.parseInt(map.get("phoneNumber").toString()),
               ActivityLevel.valueOf(map.get("activityLevel").toString()));
-	//todo return result of registering new user
 
         Map<String, String> body = new HashMap<>();
-
+        HttpHeaders header = new HttpHeaders();
+        header.add("Access-Control-Allow-Origin", "*");
+        header.add("Content-Type", "application/json; charset=UTF-8");
+        
 		if(result != null){
             body.put("id", String.valueOf(result.getUserId()));
-			return new ResponseEntity<>(body, HttpStatus.CREATED); 
+			return ResponseEntity.ok()
+            .headers(header)
+            .body(formatJson(body)); 
 		}
+
         body.put("id", "error");
 		return new ResponseEntity<>(body, HttpStatus.CONFLICT);	
     }
@@ -94,11 +101,39 @@ public class GiddController {
     public ResponseEntity loginUser(@RequestBody HashMap<String, String> map){
 		boolean result = userService.login(map.get("email").toString(), map.get("password").toString());
         Map<String, String> body = new HashMap<>();
+        HttpHeaders header = new HttpHeaders();
+        header.add("Access-Control-Allow-Origin", "*");
+        header.add("Content-Type", "application/json; charset=UTF-8");
+
         if(result){
             body.put("id", String.valueOf(userService.getUser(map.get("email")).getUserId()));
-			return new ResponseEntity<>(body, HttpStatus.OK); 
+			return ResponseEntity.ok()
+            .headers(header)
+            .body(formatJson(body));
         }
         body.put("id", "invalid password");
 		return new ResponseEntity<>(body, HttpStatus.FORBIDDEN);	
+    }
+
+    private String formatJson(Map values){
+        String result = "{";
+        Iterator it = values.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            String goose = "";
+
+            //todo very scuffed
+            try {
+                Integer.parseInt(pair.getValue().toString());
+            } catch (Exception e) {
+                goose = "\"";
+            }
+
+            System.out.println("goose: " + goose + " because " + pair.getValue() instanceof String);
+            result += "\"" + pair.getKey() + "\":" + goose + pair.getValue() + goose + ",\n";
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+        //remove trailing comma
+        return result.substring(0, result.length() - 2) + "}";
     }
 }
