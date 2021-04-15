f40b1ffe742d71e832d47064796021403aa1e345
Iben Lind Dragesund <ibenld@stud.ntnu.no>
2021-04-15 09:16:40 +0200

    fiksa activitycard

--- client/src/components/ActivityComponents/ActivityCard.tsx
+++ client/src/components/ActivityComponents/ActivityCard.tsx
@@ -1,8 +1,20 @@
-import { Card, CardActionArea, CardMedia, CardContent, Typography, Grid, Avatar, makeStyles, Theme, createStyles, Chip } from '@material-ui/core';
+import { Card, CardActionArea, CardMedia, CardContent, Typography, Grid, Avatar, Tooltip, Chip } from '@material-ui/core';
 import React from 'react';
 import styled from 'styled-components';
 import Activity from '../../interfaces/Activity';
 
+const CardInformation = styled.div`
+    height: 100%;
+
+    :hover{
+        background-color: #ebebeb
+    }
+`;
+const TitleArea = styled.div`
+    padding-left: 15px;
+    color: white;
+    background-color: #3f51b5;
+`;
 
 const ActivityCard = ({
     ID,
@@ -14,38 +26,50 @@ const ActivityCard = ({
     description,
     level,
 }: Activity) => {
+    const participants = new String(capacity);
+    const fullCapacity = new String(maxCapacity);
+    const comparison = new String(participants + "/" + fullCapacity);
+    const eventTime = new String(time);
     return (
-        <Card style={{maxWidth: '345px'}}>
-            <CardActionArea>
+        <Card style={{minWidth: '200px', maxWidth: '31%', margin: '5px'}}>
+            <CardInformation>
                 <CardMedia
                 component="img"
-                alt="Image related to the activity"
+                alt={"Image related to the activity" + title}
                 height="140"
-                image="../../assets/logo.png"
+                image ={require("../../assets/GIDD.png")}
                 />
+                    <TitleArea>
+                        <Grid container wrap="nowrap" spacing={2}>
+                            <Grid item>
+                                <Typography gutterBottom variant="h5" component="h3">
+                                    {title}
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Chip variant="outlined" size="small" label={comparison} style={{backgroundColor: "#8c98d8", borderBlockEndWidth: "0px", color: "white"}}/>
+                                <Chip variant="outlined" size="small" label={level} style={{backgroundColor: "#8c98d8", borderBlockEndWidth: "0px", color: "white"}}/>
+                            </Grid>
+                        </Grid>
+                    </TitleArea>
                 <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        Tidspunkt: {time.toDateString}
-                    </Typography>
-                    <Chip variant="outlined" size="small" label="{x}/{y}" />
-                    <Chip variant="outlined" size="small" label={level} />
-                    <Typography gutterBottom variant="h5" component="h3">
-                        {title}
-                    </Typography>
                     <Grid container wrap="nowrap" spacing={2}>
                         <Grid item>
-                            <div className="tooltip"> {owner}
+                            <Tooltip title={owner}>
                                 <Avatar>{owner.charAt(0)}</Avatar>
-                            </div>
+                            </Tooltip>
                         </Grid>
                         <Grid item xs>
                             <Typography variant="body2" color="textSecondary" component="p">
-                                {description}
+                                {eventTime}
                             </Typography>
                         </Grid>
                     </Grid>
+                    <Typography variant="body2" color="textSecondary" component="p" style={{color: 'black'}}>
+                        {description}
+                    </Typography>
                 </CardContent>
-            </CardActionArea>
+            </CardInformation>
         </Card>
     );
 };
