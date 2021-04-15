7d5f653d04aee9e069d3b342bdc6487c997a739d
erlinssl <erlinssl@stud.ntnu.no>
2021-04-15 10:15:55 +0200

    Added method for finding tag by description

--- Backend/src/main/java/IDATT2106/team6/Gidd/service/TagService.java
+++ Backend/src/main/java/IDATT2106/team6/Gidd/service/TagService.java
@@ -1,7 +1,20 @@
 package IDATT2106.team6.Gidd.service;
 
+import IDATT2106.team6.Gidd.models.Tag;
+import IDATT2106.team6.Gidd.repo.TagRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TagService {
+    @Autowired
+    private TagRepo repo;
+
+    public Tag getTag(int id){
+        return repo.findTag(id);
+    }
+
+    public Tag getTag(String desc) {
+        return repo.findTag(desc.trim());
+    }
 }
