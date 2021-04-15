a25f9ad1b4570f95f05b6368b983de9d53d21a99
Ingebrigt <ing.hovind@hotmail.com>
2021-04-15 09:03:03 +0200

    return responseentity from login

--- Backend/src/main/java/IDATT2106/team6/Gidd/controller/GiddController.java
+++ Backend/src/main/java/IDATT2106/team6/Gidd/controller/GiddController.java
@@ -2,12 +2,18 @@ package IDATT2106.team6.Gidd.controller;
 
 import IDATT2106.team6.Gidd.models.Activity;
 import IDATT2106.team6.Gidd.models.User;
-import IDATT2106.team6.gidd.service.UserService;
+import IDATT2106.team6.Gidd.service.UserService;
 import IDATT2106.team6.Gidd.service.ActivityService;
+
+import java.net.http.HttpHeaders;
 import java.util.HashMap;
+import java.util.Map;
+
 import IDATT2106.team6.Gidd.models.ActivityLevel;
 import IDATT2106.team6.Gidd.service.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,20 +30,33 @@ public class GiddController {
         return "hello world";
     }
     @PostMapping("/user")
-    public String registerUser(@RequestBody HashMap<String, String> map){
+    public ResponseEntity registerUser(@RequestBody HashMap<String, String> map){
 
-	boolean result = userService.registerUser(map.get("email").toString(),
- 32             map.get("password").toString(), map.get("firstName").toStr    ing(), map.get("surname"),
- 33             Integer.parseInt(map.get("phoneNumber").toString()),
- 34             ActivityLevel.valueOf(map.get("activityLevel").toString()));
+	User result = userService.registerUser(map.get("email").toString(),
+              map.get("password").toString(), map.get("firstName").toString(), map.get("surname"),
+              Integer.parseInt(map.get("phoneNumber").toString()),
+              ActivityLevel.valueOf(map.get("activityLevel").toString()));
 	//todo return result of registering new user
-	return "tester";
+        Map<String, String> body = new HashMap<>();
+
+		if(result != null){
+            body.put("id", String.valueOf(result.getUserId()));
+			return new ResponseEntity<>(body, HttpStatus.CREATED); 
+		}
+        body.put("id", "error");
+		return new ResponseEntity<>(body, HttpStatus.CONFLICT);	
     }
 
     @PostMapping("/login")
-    public String loginUser(@RequestBody Hashap<String, String> map){
+    public ResponseEntity loginUser(@RequestBody HashMap<String, String> map){
 		boolean result = userService.login(map.get("email").toString(), map.get("password").toString());
-	    return "login-test";
+        Map<String, String> body = new HashMap<>();
+        if(result){
+            body.put("id", String.valueOf(userService.getUser(map.get("email")).getUserId()));
+			return new ResponseEntity<>(body, HttpStatus.OK); 
+        }
+        body.put("id", "invalid password");
+		return new ResponseEntity<>(body, HttpStatus.FORBIDDEN);	
     }
 
     
