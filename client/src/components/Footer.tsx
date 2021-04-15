d65599360996d72197dcee4a0dc1ecd2dc0c8201
Mathias Myrold <mathimyr@stud.ntnu.no>
2021-04-15 12:31:23 +0200

    la til footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,23 +1,127 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
+import logo from '../assets/logo.png';
 import { AppBar, Button, Icon, Toolbar, MenuItem, makeStyles, ListItem, Drawer, TextField, TextareaAutosize } from '@material-ui/core';
+
 
 const StyledTextArea = styled.textarea`
     resize: none;
     border-radius: 10px;
 `;
 
+const StyledFooter = styled.div`
+    padding-top: 3em;
+    position: realtive;
+    bottom:  0;
+    width: 100%;
+`;
+const StyledContainer = styled.div`
+    resize: none;
+    border-radius: 10px;
+`;  
+
+const StyledRow= styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    padding: 10px;
+   
+`;
+
+const StyledCol= styled.div`
+    padding: 10px;
+    font-size: 10px;
+`;
+
+const StyledUl = styled.ul`
+     list-style: none;
+     
+`;
+
 const Footer = () => {
 
+     const [state, setState] = useState({
+    mobileView: false
+  })
+const { mobileView } = state;
+  
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 951
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+  }, []);
+
+    const displayMobile = () => {
+        return(
+             <StyledContainer>
+                     <StyledRow>
+                        <StyledCol>
+                            <Button style={{color:"white"}}>Kontakt GIDD</Button>
+                            <Button style={{color:"white"}}>Om GIDD</Button>
+                            <Button style={{color:"white"}}>Team 6</Button>
+                        </StyledCol>
+                    
+                     </StyledRow>
+
+                     <hr />
+                     <StyledRow>
+                         <p>&copy;{new Date().getFullYear} GIDD AS | Personvernerklæring og informasjonskapsler (cookies) </p>
+                     </StyledRow>
+                 </StyledContainer>
+        )
+    }
+
+    const displayDesktop = () => {
+        return(
+                 <StyledContainer>
+                     <StyledRow>
+                        <StyledCol>
+                            <StyledUl>
+                                <li>Administrerende direktør: Mathias Myrold</li>
+                                <li>Frontendutvikler: Ole</li>
+                                <li>Frontendutvikler: Håvard</li>
+                                <li>Frontendutvikler: William</li>
+                                <li>Backendutvikler: Ingebrigt</li>
+                                <li>Backendutvikler: Erling</li>
+                                <li>Backendutvikler: Iben</li>
+                                <li>Backendutvikler: Lea</li>
+                            </StyledUl>
+                        </StyledCol>
+                        <StyledCol>
+                            <StyledUl>
+                                <li ><Button style={{color:"white"}}>Kontakt GIDD</Button></li>
+                                <li ><Button style={{color:"white"}}>Om GIDD</Button></li>
+                                <li ><Button style={{color:"white"}}>Team 6</Button></li>
+                            </StyledUl>
+                        </StyledCol>
+                        <StyledCol>
+                            <StyledUl>
+                                <li>GIDD har ikke ansvar for eksterne nettsider</li>
+                                <li>som det lenkes til. Kopiering av materiale fra GIDD for</li>
+                                <li>burk av annet sted er ikke tilatt uten avtale.</li>
+                                <li> <img src={logo} style={{
+                                    width:"50px", 
+                                    margin: "10px",
+                                    cursor:"pointer"}} /></li>
+
+                            </StyledUl>
+                        </StyledCol>
+                     </StyledRow>
+
+                     <hr />
+                     <StyledRow>
+                         <p>&copy;{new Date().getFullYear} GIDD | Personvernerklæring og informasjonskapsler (cookies) </p>
+                     </StyledRow>
+                 </StyledContainer>
+        )
+    }
+
   return (
-        <AppBar position="static" style={{display:'flex', height:'300px'}}>
-            <Toolbar style={{display:"flex",flexDirection:"column", justifyContent:"space-between"}}>
-                <h3>Gi oss tilbake meldinger!</h3>
-                <div>
-                    <StyledTextArea wrap={'off'} cols={70} rows={10}/>
-                </div>
-                <Button style={{backgroundColor:'white'}}>Send tilbakemeldinger</Button>
-            </Toolbar>
+        <AppBar position="static" style={{}}>
+            {mobileView ? displayMobile() : displayDesktop()}
         </AppBar>
   );
 }
