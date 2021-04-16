0c37336e5d97135414f96d965656f5758bf7577c
haavarty <haavarty@stud.ntnu.no>
2021-04-16 09:18:50 +0200

    Login and register with backend

---
+++ client/src/containers/Calender.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import {ActivityList} from '../interfaces/Activity'
+
+
+
+
+const Calender = () =>  {
+
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Calender;
