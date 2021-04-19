cc9c40eceddcdb536f5f894dc71962d6f40994a6
Iben Lind Dragesund <ibenld@stud.ntnu.no>
2021-04-19 16:07:11 +0200

    siste update for dagen

--- client/src/components/WeatherComponents/WeatherComponent.tsx
+++ client/src/components/WeatherComponents/WeatherComponent.tsx
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import WeatherContent from './WeatherContent';
+/* denne script-fila må hente ut alle data for været og 
+sende det til ein weatherContent div*/
+
+/* <WeatherContent
+      weatherData={weather}
+    />
+*/
 
 interface Props{
   lat: number;
@@ -7,8 +15,13 @@ interface Props{
 }
 
 const WeatherComponent = ({ lat, lon, time }: Props) =>{
+  const openWeatherURL = "http://api.openweathermap.org/data/2.5/forecast?";
+  const lat_long = "lat=" + lat.toString + "&lon=" + lon.toString;
+  const join_key = "&appid=" + "b6907d289e10d714a6e88b30761fae22";
+  const units = "&units=metric";
+
   return(
-    <div>HI!</div>
+    <div>HI</div>
   )
 }
 
