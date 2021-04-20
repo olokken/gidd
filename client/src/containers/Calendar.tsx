741cf7ca824cd486a52e35a446e97cf3cd251083
haavarty <haavarty@stud.ntnu.no>
2021-04-20 16:03:43 +0200

    Calender and login working

--- client/src/containers/Calendar.tsx
+++ client/src/containers/Calendar.tsx
@@ -50,17 +50,10 @@ const Calender = () => {
   }
   );
 
-  const getMyActivities = () => {
-    const url = `/user/${user}/activity`;
-    axios.get(url).then((response) => {
-      console.log(response.data);
-    }).catch((error) => {
-      console.log('error' + error.message)
-    })
-  }
 
   useEffect(() => {
-    const url = '/activity'
+    const id = localStorage.getItem('userID')
+    const url = `/user/${id}/activity`;
     axios.get(url).then((response) => {
       console.log(response.data['activities'])
       response.data['activities'].forEach((activity: any) => {
