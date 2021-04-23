b9614653a52078d00d7d0aa8d3c65b52783cf370
williamforbrigd <hansw0701@gmail.com>
2021-04-23 11:42:06 +0200

    pulled data from backend

--- client/src/components/LeaderboardComponents/GroupLeaderboard.tsx
+++ client/src/components/LeaderboardComponents/GroupLeaderboard.tsx
@@ -1,5 +1,5 @@
 import axios from '../../Axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Group from '../../interfaces/Group';
 import User from '../../interfaces/User';
 import { Avatar, Card, CardContent, makeStyles } from '@material-ui/core';
@@ -23,63 +23,68 @@ const useStyles = makeStyles({
     },
 });
 interface Props {
-    group?: Group;
+    group: Group;
 }
 
 export const GroupLeaderboard: React.FC<Props> = ({ group }: Props) => {
     const classes = useStyles();
-    const [members, setMembers] = useState<User[]>([]);
-    //TODO work with this method.
-    const getPoints = async () => {
-        //TODO: the url get the group.
-        const request = await axios.get('/group');
+    const [users, setUsers] = useState<User[]>([]);
+
+    const filterPoints = (): User[] => {
+        return users.sort((u1, u2) => +u1.points - +u2.points);
     };
 
-    /*
-    const filterPoints = () => {
-        return members.sort((m1, m2) => m1.points - m2.points);
+    const getTotalPoints = (): number => {
+        let sum = 0;
+        users.forEach((user) => {
+            sum += +user.points;
+        });
+        return sum;
     };
-
-    const getTotalPoints = () => {
-        return members.reduce((m1, m2) => m1.points + m2.points);
-    };
-    */
 
     //The max margin-left is 52rem
-    const getMarginLeft1 = () => {
-        //(userpoints/totalpoints)%52
-        return ((100 / 150) * 52) % 52;
+    const getMarginLeft = (user: User) => {
+        return ((+user.points / getTotalPoints()) * 52) % 52;
     };
-    const getMarginLeft2 = () => {
-        //(userpoints/totalpoints)%52
-        return ((50 / 150) * 52) % 52;
+
+    const getPoints = async () => {
+        const request = await axios.get(`/group/${group.groupId}`);
+        console.log(request);
+        return request;
     };
+
+    useEffect(() => {
+        getPoints();
+    }, []);
 
     return (
         <Card className={classes.root}>
             <div className="groupleaderboard__header">
-                <h1>{group ? group.name : 'hei'}</h1>
+                <h1>{group.name}</h1>
             </div>
-            <hr className="groupleaderboard__line" />
-            <div className="groupleaderboard__player">
-                <div
-                    /* The max margin-left is 52rem*/
-                    style={{
-                        margin: `0.7rem 0.5rem 0.5rem ${getMarginLeft1()}rem`,
-                        flex: '1',
-                    }}
-                >
-                    <div className="groupleaderboard__logo">
-                        <h6 className="groupleaderboard__name">William</h6>
-                        <Avatar />
+            {users.map((user) => {
+                <hr className="groupleaderboard__line" />;
+                <div className="groupleaderboard__player">
+                    <div
+                        /* The max margin-left is 52rem*/
+                        style={{
+                            margin: `0.7rem 0.5rem 0.5rem ${getMarginLeft(
+                                user
+                            )}rem`,
+                            flex: '1',
+                        }}
+                    >
+                        <div className="groupleaderboard__logo">
+                            <h6 className="groupleaderboard__name">William</h6>
+                            <Avatar />
+                        </div>
                     </div>
-                </div>
-                <div className="groupleaderboard__stats">
-                    <h5 className="groupleaderboard__stat1">3rd Place</h5>
-                    <h5 className="groupleaderboard__stat2">100 points</h5>
-                </div>
-            </div>
-            <hr className="groupleaderboard__line" />
+                    <div className="groupleaderboard__stats">
+                        <h5 className="groupleaderboard__stat1">3rd Place</h5>
+                        <h5 className="groupleaderboard__stat2">100 points</h5>
+                    </div>
+                </div>;
+            })}
         </Card>
     );
 };
--- client/src/containers/Leaderboard.tsx
+++ client/src/containers/Leaderboard.tsx
@@ -1,78 +1,29 @@
-import React from 'react';
+import axios from '../Axios';
+import React, { useEffect, useState } from 'react';
 import { GroupLeaderboard } from '../components/LeaderboardComponents/GroupLeaderboard';
+import Group from '../interfaces/Group';
 
 function Leaderboard() {
+    const [groups, setGroups] = useState<Group[]>([]);
+
+    const getAllGroups = async () => {
+        const request = await axios.get('/group');
+        console.log(request);
+        setGroups(request.data.groups);
+        return request;
+    };
+
+    useEffect(() => {
+        getAllGroups();
+    }, []);
+
     return (
         <div style={{ margin: '1rem' }}>
-            <GroupLeaderboard />
-            <GroupLeaderboard />
-            <GroupLeaderboard />
-            <GroupLeaderboard />
-            <GroupLeaderboard />
-            <GroupLeaderboard />
+            {groups.map((group) => {
+                <GroupLeaderboard group={group} />;
+            })}
         </div>
     );
 }
 
 export default Leaderboard;
-
-/*
-import React, {
-    ChangeEvent,
-    KeyboardEventHandler,
-    useEffect,
-    useState,
-} from 'react';
-import ActivityForm from '../components/ActivityComponents/ActivityForm';
-import styled from 'styled-components';
-import SideFilter from '../components/FilterComponents/SideFilter';
-import SortMenu from '../components/SortingComponents/SortMenu';
-import Popup from '../components/Popup'
-import AddButton from '../components/ActivityComponents/AddButton'; 
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-
-//Endringer kan forekomme her
-
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }),
-);
-
-const Leaderboard = () => {
-    const classes = useStyles();
-
-    return (
-        <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={9}>
-          <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-          
-          <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-        </Grid>
-        
-        
-      </Grid>
-    </div>
-    );
-};
-
-export default Leaderboard;
-*/
