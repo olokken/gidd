af1b90dcb45dc89f85be9e9d654e970329beaa44
ikhovind <ikhovind@stud.ntnu.no>
2021-04-27 08:54:20 +0200

    cleanup + made saving message thread safe

--- Backend/src/main/java/IDATT2106/team6/Gidd/web/ChatController.java
+++ Backend/src/main/java/IDATT2106/team6/Gidd/web/ChatController.java
@@ -2,11 +2,9 @@ package IDATT2106.team6.Gidd.web;
 
 import IDATT2106.team6.Gidd.models.Activity;
 import IDATT2106.team6.Gidd.models.Chat;
-import IDATT2106.team6.Gidd.models.FriendGroup;
 import IDATT2106.team6.Gidd.models.User;
 import IDATT2106.team6.Gidd.service.*;
 import IDATT2106.team6.Gidd.util.Logger;
-import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
@@ -22,7 +20,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -47,20 +44,21 @@ public class ChatController {
 
     @GetMapping(value = "/chat/{groupId}", produces = "application/json")
     public ResponseEntity getMessageLog(@PathVariable Integer groupId){
-        ArrayList<Chat> messages = new ArrayList<>();
         HttpHeaders header = new HttpHeaders();
+
         Activity activity = activityService.getActivity(groupId);
         HashMap<String, String> body = new HashMap<>();
         if(activity != null) {
             List<Chat> messageList = chatService.getMessages(activity);
             if (messageList != null) {
                 StringBuilder messageJson = new StringBuilder();
-                messageJson.append("{\"activity\":" + "\"" + groupId + "\",");
-                messageJson.append("\"messages\" : [");
+                messageJson.append("{\"activity\":" + "\"").append(groupId)
+                        .append("\",").append("\"messages\" : [");
                 for (Chat c : messageList){
-                    messageJson.append(c.toJson() + ",");
+                    messageJson.append(c.toJson()).append(",");
                 }
                 messageJson.append("]");
+                //trailing comma
                 messageJson.deleteCharAt(messageJson.length() - 2);
                 return ResponseEntity
                         .ok()
@@ -82,16 +80,17 @@ public class ChatController {
 
     @MessageMapping("/chat/{activityId}")
     public void sendMessage(@DestinationVariable Integer activityId, @Payload String message) throws ParseException {
+        log.info("recieved chat message to id: " + activityId + " with message: " + message);
+
         JSONParser parser = new JSONParser();
-        System.out.println("message is: " + message);
         JSONObject chatJson = (JSONObject) parser.parse(message);
+
         Activity activity = activityService.getActivity(activityId);
         User user = userService.getUser(chatJson.getAsNumber("userId").intValue());
 
         chatJson.put("user", user);
         chatJson.remove("userId");
-        HashMap<String, String> response = new HashMap<>();
-        response.put("user", user.toJSON());
+
         Chat newChat = new Chat(activity, user, String.valueOf(chatJson.get("message")));
         String json = "{" +
                 "\"user\":" + user.toJSON() +
@@ -99,10 +98,15 @@ public class ChatController {
                 ",\"timestamp\":" + "\"" + newChat.getTimeStamp().toString() + "\"" +
                 "}";
 
-                //todo make thread safe and ensure that id does not exist
-        newChat.setChatId(getRandomID());
-        if(chatService.saveChat(newChat)){
-            System.out.println(json);
+        int counter = 0;
+        boolean succeeded;
+        do{
+            newChat.setChatId(getRandomID());
+            //avoid infinte loop incase of database error
+            succeeded = chatService.saveChat(newChat);
+        } while(counter++ < 5 && !succeeded);
+        //if the counter did not reach threshold, save chat succeeded
+        if (succeeded) {
             template.convertAndSend("/client/chat/" + activityId, json);
         }
         else {
